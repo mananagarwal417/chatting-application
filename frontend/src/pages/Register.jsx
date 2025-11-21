d7847c3b1abfb4d4a2cf10dbc0fa8489222b4ba0
agarwal1771 <agarwalmanan783@gmail.com>
2025-11-21 13:04:02 +0530

    Fix frontend changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -57,7 +57,7 @@ function Register() {
           </p>
         </div>
 
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form className="mt-8 space-y-6" onClick={handlesubmit}>
           <div className="rounded-md shadow-sm space-y-4">
 
             {/* USERNAME */}
@@ -146,7 +146,7 @@ function Register() {
           <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.95 }}>
             <button
               type="submit"
-              onClick={handlesubmit}
+              
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent 
               text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 
               focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
