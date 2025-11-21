5685d7938f616c23fd0aa5291fb331ff42c5a335
agarwal1771 <agarwalmanan783@gmail.com>
2025-11-21 14:11:26 +0530

    Fix frontend changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,6 +26,8 @@ function Login() {
       navigate("/dashboard");
     } catch (err) {
       alert(err.response?.data?.message || "Login failed");
+      setEmail("");
+      setPassword("");
     }
   };
 
